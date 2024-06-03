c03f86182515306c2560fcf413829971016405d6
max <maxpete121@gmail.com>
2024-06-03 16:04:41 -0600

    Phone view

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -2,44 +2,79 @@
 
 <template>
   <section class="container-fluid">
-    <div class="row justify-content-lg-center info-container">
-      <div class="col-lg-7 col-11 p-4">
-        <div class="d-lg-flex justify-content-evenly">
-          <div class="d-flex flex-column align-items-center me-4">
+    <div class="row justify-content-center info-container">
+      <div class="col-lg-7 col-11 p-4 d-flex justify-content-center justify-content-lg-evenly">
+        <div class="d-flex justify-content-lg-evenly justify-content-center top-container">
+          <div class="d-flex flex-column align-items-center me-lg-4 text-center">
             <div class="d-inline-block title-box ps-2 pe-2">
               <h2 class="fst-italic main-font">Maxwell Petersen</h2>
             </div>
-            <img class="image mt-3" src="../assets/img/unnamed.jpg" alt="">
+            <div>
+              <img class="image mt-3" src="../assets/img/unnamed.jpg" alt="">
+            </div>
           </div>
-          <div class="ms-4 text-center about-parent">
+          <div class="ms-lg-4 mt-3 text-center about-parent">
             <h3 class="fst-italic title-font">Software Engineer</h3>
-            <div class="d-flex p-1 justify-content-center mt-3">
+            <div class="d-flex p-1 justify-content-center mt-4">
               <button class="contact-btn">Contact Me</button>
               <button class="project-btn">Projects <i class="mdi mdi-arrow-top-right-thick"></i></button>
             </div>
-            <div class=" p-1 mt-3">
-              
+            <div class="p-1 mt-4">
+              <h4 class="title-font ps-1 pe-1">Skills</h4>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">Javascript</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">HTML</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">CSS</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">C#</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">MYSQL</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">Web APIs</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">Figma</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">Bootstrap</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">.NET Core</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">MVC</h6>
+              </div>
+              <div class="skill-box d-inline-block m-1">
+                <h6 class="fst-italic">VUE</h6>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <div class="row justify-content-center pt-4">
-      <div class="col-7">
-        <div class="d-flex justify-content-center">
+    <div class="row justify-content-center pt-4 mb-4">
+      <div class="col-12 col-lg-7">
+        <div class="d-lg-flex justify-content-center">
           <div class="d-flex flex-column me-lg-4">
             <div class="text-center">
-              <h3 class="title d-inline-block title-font">My Projects</h3>
+              <h3 class="title d-inline-block title-font mt-3">My Projects</h3>
             </div>
             <div class="mt-2 d-flex flex-column align-items-center">
-              <div class="d-flex ">
+              <div class="d-flex justify-content-center">
                 <button @click="changeProjectInfo(0)" class="filter-btn">In Development</button>
                 <button @click="changeProjectInfo(1)" class="filter-btn">KeeprCo</button>
                 <button @click="changeProjectInfo(2)" class="filter-btn">All Spice</button>
               </div>
               <div class="project-info ps-2 pe-2 pt-3">
                 <div class="text-center">
-                  <h4 class="fst-italic">{{ projectData.name }}</h4>
+                  <h4 class="fst-italic mt-2 mb-2">{{ projectData.name }}</h4>
                 </div>
                 <div class="language-one d-flex align-items-baseline justify-content-center pt-3 pb-2">
                   <h5>Languages:</h5>
@@ -51,24 +86,18 @@
                     <h6>{{ projectData.info }}</h6>
                   </div>
                 </div>
-                <div class="language-one mt-2 justify-content-center">
+                <div class="language-one mt-2 justify-content-center text-center">
                   <a :href="projectData.link" target="_blank">Explore</a>
                 </div>
               </div>
             </div>
           </div>
-          <div class="text-center ms-lg-4">
-            <h3 class="title d-inline-block title-font">My Goals</h3>
-            <div class="mt-2">
-              <button @click="changeGoalsInfo(0)" class="filter-btn">Career</button>
-              <button @click="changeGoalsInfo(1)" class="filter-btn">Personal</button>
-            </div>
-            <div class="project-info">
-              <h4>{{ goalsData.infoOne }}</h4>
-            </div>
-          </div>
         </div>
       </div>
+    </div>
+    <div class="row">
+      <div class="col-4"></div>
+      <div class="col-4"></div>
     </div>
   </section>
 </template>
@@ -113,6 +142,39 @@ async function changeGoalsInfo(index){
 
 </script>
 <style scoped lang="scss">
+
+@media screen and (min-width: 576px) {
+  .about-parent{
+    width: 300px;
+  }
+
+  .project-info{
+  background-color: #c4c1bf3a;
+  max-width: 400px;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.24);
+  border-left: solid 2px rgb(49, 215, 237);
+}
+}
+
+@media screen and (max-width: 576px) {
+  .about-parent{
+    width: 260px;
+  }
+
+  .top-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .project-info{
+  background-color: #c4c1bf3a;
+  max-width: 357px;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.24);
+  border-left: solid 2px rgb(49, 215, 237);
+}
+}
+
 .title-font{
   font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
   color: black;
@@ -155,11 +217,6 @@ async function changeGoalsInfo(index){
 
 .language-one{
   border-top: solid 1px rgb(49, 215, 237);
-}
-
-.project-info{
-  background-color: #4f4a453a;
-  max-width: 400px;
 }
 
 .contact-btn{
@@ -235,17 +292,19 @@ async function changeGoalsInfo(index){
   padding-bottom: 6px;
   padding-left: 20px;
   padding-right: 20px;
-  background-color: #4f4a453a;
+  background-color: #c4c1bf3a;
 }
 
 .skill-box{
-  outline: solid 1px black;
   color: black;
-  background-color: #ED7D31;
+  background-color: rgb(51, 227, 250);
   border-radius: 10px;
-  width: 100px;
-  height: 20px;
-  margin-top: 4px;
+  max-width: 100px;
+  height: 25px;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 2px;
+  padding-bottom: 5px;
 }
 
 .career-button-section{
